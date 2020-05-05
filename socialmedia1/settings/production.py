146eef3f2dee4844207375a368952ba490b53902
Muhammed Ali <muhamzyali@gmail.com>
2020-05-05 21:58:35 +0100

    Update production

--- socialmedia1/settings/production.py
+++ socialmedia1/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0r9s=y0@smv1&06oa(dps-o91(pha__=@fr73xfx#1c!2+w&2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
